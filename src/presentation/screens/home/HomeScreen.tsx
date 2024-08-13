4a34d53d8a90e6cbe26f785fb2dad7ccd935d155
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-08-13 01:36:48 -0500

    179. Menú principal y estilos

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,10 +1,83 @@
-import { View } from 'react-native';
-import { AirplaneIcon } from '../../icons/Icons';
+import { Text, View } from 'react-native';
+import { globalStyles } from '../../../config/theme/theme';
+import { ScrollView } from 'react-native-gesture-handler';
+import { Title } from '../../components/ui/Title';
+
+export const menuItems = [
+	// 01-animationMenuItems
+	{
+		name: 'Animation 101',
+		icon: 'cube-outline',
+		component: 'Animation101Screen'
+	},
+	{
+		name: 'Animation 102',
+		icon: 'albums-outline',
+		component: 'Animation102Screen'
+	},
+
+	// 02-menuItems
+	{
+		name: 'Pull to refresh',
+		icon: 'refresh-outline',
+		component: 'PullToRefreshScreen'
+	},
+	{
+		name: 'Section List',
+		icon: 'list-outline',
+		component: 'CustomSectionListScreen'
+	},
+	{
+		name: 'Modal',
+		icon: 'copy-outline',
+		component: 'ModalScreen'
+	},
+	{
+		name: 'InfiniteScroll',
+		icon: 'download-outline',
+		component: 'InfiniteScrollScreen'
+	},
+	{
+		name: 'Slides',
+		icon: 'flower-outline',
+		component: 'SlidesScreen'
+	},
+	{
+		name: 'Themes',
+		icon: 'flask-outline',
+		component: 'ChangeThemeScreen'
+	},
+
+	// 03- uiMenuItems
+	{
+		name: 'Switches',
+		icon: 'toggle-outline',
+		component: 'SwitchScreen'
+	},
+	{
+		name: 'Alerts',
+		icon: 'alert-circle-outline',
+		component: 'AlertScreen'
+	},
+	{
+		name: 'TextInputs',
+		icon: 'document-text-outline',
+		component: 'TextInputScreen'
+	}
+];
 
 export const HomeScreen = () => {
 	return (
-		<View>
-			<AirplaneIcon />
+		<View style={[globalStyles.mainContainer]}>
+			<View style={globalStyles.globalMargin}>
+				<ScrollView>
+					<Title text="Opciones del menú" safe />
+
+					{menuItems.map(item => (
+						<Text key={item.component}>{item.name}</Text>
+					))}
+				</ScrollView>
+			</View>
 		</View>
 	);
 };
