e93a3b34ddb99afcdbd900db50f4c5d12bebac66
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-08-25 00:15:30 -0500

    187. Componente - Switch

---
+++ src/presentation/components/ui/CustomSwitch.tsx
@@ -0,0 +1,33 @@
+import { Platform, StyleSheet, Text, View } from 'react-native';
+import { Switch } from 'react-native-gesture-handler';
+import { colors } from '../../../config/theme/theme';
+
+interface Props {
+	isOn: boolean;
+	text?: string;
+	onChange: (value: boolean) => void;
+}
+
+export const CustomSwitch = ({ isOn, text, onChange }: Props) => {
+	return (
+		<View style={styles.switchRow}>
+			{text && <Text style={{ color: colors.text }}>{text}</Text>}
+
+			<Switch
+				thumbColor={Platform.OS === 'android' ? colors.primary : ''}
+				ios_backgroundColor="#3e3e3e"
+				onValueChange={onChange}
+				value={isOn}
+			/>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({
+	switchRow: {
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+		marginVertical: 5
+	}
+});
--- src/presentation/screens/switches/SwitchScreen.tsx
+++ src/presentation/screens/switches/SwitchScreen.tsx
@@ -1,7 +1,36 @@
+import { useState } from 'react';
 import { CustomView } from '../../components/ui/CustomView';
+import { Card } from '../../components/ui/Card';
+import { CustomSwitch } from '../../components/ui/CustomSwitch';
 
 export const SwitchScreen = () => {
+	const [state, setState] = useState({
+		isActive: false,
+		isHungry: false,
+		isHappy: false
+	});
+
 	return (
-		<CustomView style={{ marginTop: 100, paddingHorizontal: 10 }}></CustomView>
+		<CustomView style={{ marginTop: 100, paddingHorizontal: 10 }}>
+			<Card>
+				<CustomSwitch
+					isOn={state.isActive}
+					onChange={value => setState({ ...state, isActive: value })}
+					text="¿Está Activo?"
+				/>
+
+				<CustomSwitch
+					isOn={state.isHungry}
+					onChange={value => setState({ ...state, isHungry: value })}
+					text="¿Tiene hambre?"
+				/>
+
+				<CustomSwitch
+					isOn={state.isHappy}
+					onChange={value => setState({ ...state, isHappy: value })}
+					text="¿Es feliz?"
+				/>
+			</Card>
+		</CustomView>
 	);
 };
