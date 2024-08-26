548c1e1bf33fd418a3f5fed811922a766d295be7
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-08-26 12:15:41 -0500

    194. Pull to refresh

---
+++ src/presentation/components/ui/PullToRefreshScreen.tsx
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import { Title } from './Title';
+import { ScrollView } from 'react-native-gesture-handler';
+import { RefreshControl } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { colors, globalStyles } from '../../../config/theme/theme';
+// import { CustomView } from './CustomView';
+
+export const PullToRefreshScreen = () => {
+	const [isRefreshing, setIsRefreshing] = useState(false);
+
+	const { top } = useSafeAreaInsets();
+
+	const onRefresh = () => {
+		setIsRefreshing(true);
+
+		setTimeout(() => {
+			setIsRefreshing(false);
+		}, 4500);
+	};
+
+	return (
+		<ScrollView
+			refreshControl={
+				<RefreshControl
+					refreshing={isRefreshing}
+					progressViewOffset={top}
+					colors={[colors.primary, 'red', 'orange', 'green']}
+					onRefresh={onRefresh}
+				/>
+			}
+			style={[globalStyles.mainContainer, globalStyles.globalMargin]}>
+			{/* <CustomView margin> */}
+			<Title text="Pull to refresh" safe />
+			{/* </CustomView> */}
+		</ScrollView>
+	);
+};
--- src/presentation/navigator/Navigator.tsx
+++ src/presentation/navigator/Navigator.tsx
@@ -5,6 +5,7 @@ import { Animation102Screen } from '../screens/animations/Animation102Screen';
 import { SwitchScreen } from '../screens/switches/SwitchScreen';
 import { AlertScreen } from '../screens/alerts/AlertScreen';
 import { TextInputScreen } from '../screens/inputs/TextInputScreen';
+import { PullToRefreshScreen } from '../components/ui/PullToRefreshScreen';
 
 const Stack = createStackNavigator();
 
@@ -17,6 +18,10 @@ export const Navigator = () => {
 			<Stack.Screen name="SwitchScreen" component={SwitchScreen} />
 			<Stack.Screen name="AlertScreen" component={AlertScreen} />
 			<Stack.Screen name="TextInputScreen" component={TextInputScreen} />
+			<Stack.Screen
+				name="PullToRefreshScreen"
+				component={PullToRefreshScreen}
+			/>
 		</Stack.Navigator>
 	);
 };
