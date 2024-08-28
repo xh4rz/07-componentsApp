5726d819401c2f18fdd333cde2f2a9d98af87d89
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-08-28 11:49:05 -0500

    208. Theme Light y Dark - Generalidades

--- src/presentation/navigator/Navigator.tsx
+++ src/presentation/navigator/Navigator.tsx
@@ -10,6 +10,7 @@ import { CustomSectionListScreen } from '../screens/ui/CustomSectionListScreen';
 import { ModalScreen } from '../screens/ui/ModalScreen';
 import { InfiniteScrollScreen } from '../screens/ui/InfiniteScrollScreen';
 import { SlidesScreen } from '../screens/ui/SlidesScreen';
+import { ChangeThemeScreen } from '../screens/theme/ChangeThemeScreen';
 
 const Stack = createStackNavigator();
 
@@ -36,6 +37,7 @@ export const Navigator = () => {
 				component={InfiniteScrollScreen}
 			/>
 			<Stack.Screen name="SlidesScreen" component={SlidesScreen} />
+			<Stack.Screen name="ChangeThemeScreen" component={ChangeThemeScreen} />
 		</Stack.Navigator>
 	);
 };
