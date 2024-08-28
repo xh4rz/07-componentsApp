e0423b39942941bdad405f44569aa52ef102a6bd
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-08-28 02:47:04 -0500

    200. Cierre de sección

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -88,23 +88,23 @@ export const HomeScreen = () => {
 
 					<View style={{ marginTop: 30 }} />
 
-					{menuItems.map((item, index) => (
-						<MenuItem
-							key={item.component}
-							{...item}
-							isFirst={index === 0}
-							isLast={index === menuItems.length - 1}
-						/>
-					))}
-
-					<View style={{ marginTop: 30 }} />
-
 					{uiMenuItems.map((item, index) => (
 						<MenuItem
 							key={item.component}
 							{...item}
 							isFirst={index === 0}
 							isLast={index === uiMenuItems.length - 1}
+						/>
+					))}
+
+					<View style={{ marginTop: 30 }} />
+
+					{menuItems.map((item, index) => (
+						<MenuItem
+							key={item.component}
+							{...item}
+							isFirst={index === 0}
+							isLast={index === menuItems.length - 1}
 						/>
 					))}
 
