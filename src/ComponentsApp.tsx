bca6ed6b17c76c1b56b8c9b2df6d04645e8754e5
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-08-30 02:22:31 -0500

    212. React Navigation Theme

--- src/ComponentsApp.tsx
+++ src/ComponentsApp.tsx
@@ -1,21 +1,38 @@
-import { PropsWithChildren } from 'react';
+import { PropsWithChildren, useContext } from 'react';
 import 'react-native-gesture-handler';
-import { NavigationContainer } from '@react-navigation/native';
+import {
+	DarkTheme,
+	DefaultTheme,
+	NavigationContainer
+} from '@react-navigation/native';
 import { Navigator } from './presentation/navigator/Navigator';
-import { ThemeProvider } from './presentation/context/ThemeContext';
+import {
+	ThemeContext,
+	ThemeProvider
+} from './presentation/context/ThemeContext';
 
-const AppState = ({ children }: PropsWithChildren) => {
+const AppNavigation = ({ children }: PropsWithChildren) => {
+	const { isDark } = useContext(ThemeContext);
+
 	return (
-		<NavigationContainer>
-			<ThemeProvider>{children}</ThemeProvider>
+		<NavigationContainer theme={isDark ? DarkTheme : DefaultTheme}>
+			{children}
 		</NavigationContainer>
+	);
+};
+
+const AppTheme = ({ children }: PropsWithChildren) => {
+	return (
+		<ThemeProvider>
+			<AppNavigation>{children}</AppNavigation>
+		</ThemeProvider>
 	);
 };
 
 export const ComponentsApp = () => {
 	return (
-		<AppState>
+		<AppTheme>
 			<Navigator />
-		</AppState>
+		</AppTheme>
 	);
 };
